30f13212a79f64022cae73d5a459f6d146f2f191
Duran <jose.duran@uamv.edu.ni>
2026-05-29 12:35:23 -0600

    Agregue la funcion menu y main

--- RegistroEmpleado/Program.cs
+++ RegistroEmpleado/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using Microsoft.VisualBasic;
+
 Empleado[] empleados = new Empleado[10];
 
 void agregarEmpleado(int pos)
@@ -9,12 +11,40 @@ void agregarEmpleado(int pos)
     empleados[pos].apellidos = Console.ReadLine();
     Console.Write("Cargo: ");
     empleados[pos].cargo = Console.ReadLine();
-    Console.WriteLine("Salario: ");
+    Console.Write("Salario: ");
     empleados[pos].salario = Double.Parse(Console.ReadLine());
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Registro guardado satisfactoriamente");
     Console.ResetColor();
 }
+
+int menu()
+{
+    Console.Write("1. Agregar \n2. Mostrar \n3. Eliminar \n4.Salir \nDigite su opcion: ");
+    int op = int.Parse(Console.ReadLine());
+    return op;
+}
+
+int main()
+{
+    int op = 0, i = 0;
+
+    do
+    {
+        Console.WriteLine($"Registro # {i + 1}");
+        op = menu();
+        switch (op)
+        {
+            case 1:
+                agregarEmpleado(i++);
+                break;
+        }
+    } while (op != 4);
+
+    return 0;
+}
+
+main();
 
 
 struct Empleado
